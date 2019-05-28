6d4d4ac3a8dbb2e83a993f1653fe4ac2f4203eda
BhaaL <bhaal@0x1337.org>
2019-05-28 18:13:23 +0200

    enable BackgroundLoad on the AutoLoad attribute

--- VSXPackage.cs
+++ VSXPackage.cs
@@ -43,7 +43,7 @@ namespace GitHub.BhaaLseN.VSIX
     /// </remarks>
     [MVSS.PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [MVSS.InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
-    [MVSS.ProvideAutoLoad(VSConstants.UICONTEXT.NoSolution_string)]
+    [MVSS.ProvideAutoLoad(VSConstants.UICONTEXT.NoSolution_string, MVSS.PackageAutoLoadFlags.BackgroundLoad)]
     [MVSS.ProvideMenuResource("Menus.ctmenu", 1)]
     [Guid(PackageGuidString)]
     [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
